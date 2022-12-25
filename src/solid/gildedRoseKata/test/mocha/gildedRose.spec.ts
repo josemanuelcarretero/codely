8196e60ba31fde37e7dd6f9716ef4baa137f633d
Jose Manuel Carretero <josemanuelcarreterocuenca@gmail.com>
2022-12-25 20:05:03 +0100

    Add gilded rose kata from original repository: https://github.com/emilybache/GildedRose-Refactoring-Kata

---
+++ src/solid/gildedRoseKata/test/mocha/gildedRose.spec.ts
@@ -0,0 +1,10 @@
+import { GildedRose, Item } from '@codely/gildedRose';
+import { expect } from 'chai';
+
+describe('Gilded Rose', () => {
+  it('should foo', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal('fixme');
+  });
+});
