28f7509e0b90b3a930706cc81bdfce0a33db08da
Jose Manuel Carretero <josemanuelcarreterocuenca@gmail.com>
2022-12-25 21:51:51 +0100

    Create constant from strings inside code

--- src/solid/gildedRoseKata/src/gildedRose.ts
+++ src/solid/gildedRoseKata/src/gildedRose.ts
@@ -1,5 +1,9 @@
 import { Item } from '@codely/item';
 
+const AGED_BRIE = 'Aged Brie';
+const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -10,20 +14,18 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].name != 'Aged Brie' &&
-        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
+        this.items[i].name != AGED_BRIE &&
+        this.items[i].name != BACKSTAGE_PASSES
       ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != SULFURAS) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
+          if (this.items[i].name == BACKSTAGE_PASSES) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -37,16 +39,14 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != SULFURAS) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (
-            this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
+        if (this.items[i].name != AGED_BRIE) {
+          if (this.items[i].name != BACKSTAGE_PASSES) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != SULFURAS) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
